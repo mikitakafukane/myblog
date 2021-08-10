7b88fdd1950e6bb560aebd980e7eef7252a505e2
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-08-10 15:13:10 +0900

    finish18

--- index.php
+++ index.php
@@ -78,7 +78,10 @@
           <div class="post-preview">
             <a href="post.html">
               <h2 class="post-title">
-                <?php the_title(); ?>
+                <?php while (have_posts()) : ?>
+                  <?php the_post(); ?>
+                  <?php the_title(); ?>
+                <?php endwhile; ?>
               </h2>
               <h3 class="post-subtitle">
                 Problems look mighty small from 150 miles up
