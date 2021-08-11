56afe517fb916503880209ef1bfbf9d0a44be62c
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-08-11 23:04:56 +0900

    finish30

--- page.php
+++ page.php
@@ -2,7 +2,9 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+
   <?php get_header(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -21,35 +23,38 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
-              <div class="post-heading">
+              <div class="page-heading">
                 <h1><?php the_title(); ?></h1>
-                <span class="meta">Posted by
-                  <?php the_author(); ?>
-                  on <?php the_date(); ?></span>
               </div>
             </div>
           </div>
         </div>
       </header>
 
-      <!-- Post Content -->
-      <article>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-              <?php the_content(); ?>
-            </div>
+      <!-- Main Content -->
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <?php the_content(); ?>
           </div>
         </div>
-      </article>
+      </div>
 
       <hr>
+
     <?php endwhile; ?>
   <?php endif; ?>
 
   <?php get_template_part('includes/footer'); ?>
 
   <?php get_footer(); ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
 
 </body>
 
